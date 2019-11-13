6ed49fcc6f5354d9b399574d8b14ad44bd175e13
Santosh Kanakagiri <santosh.k@licious.in>
2019-11-13 12:18:17 +0530

    KthSmallestElementInSortedMatrix

---
+++ src/com/santosh/practise/arrays/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,78 @@
+package com.santosh.practise.arrays;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * LeetCode problem 378
+ */
+public class KthSmallestElementInSortedMatrix {
+    public static void main(String[] args) {
+        int n = 3;
+        int k = 3;
+        int[][] matrix = {{1,  5,  9},{10, 11, 13},{12, 13, 15}};
+        int kThSmallestElement = getKthSmallestElementFromSortedMartix(matrix, k);
+        System.out.println("K th Smallest Element is::" + kThSmallestElement);
+    }
+
+    private static int getKthSmallestElementFromSortedMartix(int[][] matrix, int k) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        int[][] visited = new int[numRows][numCols];
+
+        int removedCount = 0;
+        PriorityQueue<HeapNode> minHeap = new PriorityQueue<HeapNode>((o1, o2) -> o1.data - o2.data);
+        minHeap.add(new HeapNode(matrix[0][0], 0, 0));
+        visited[0][0] = 1;
+        HeapNode lastRemovedElement = null;
+        while(removedCount != k) {
+            HeapNode currentMinimumElement = minHeap.poll();
+            int currentRow = currentMinimumElement.row;
+            int currentColumn = currentMinimumElement.column;
+            lastRemovedElement = currentMinimumElement;
+            removedCount++;
+            System.out.println("Removing:: " + currentMinimumElement.data);
+            boolean isElementOnRightValidAndNotVisited = isElementOnRightValid(currentMinimumElement, numRows, visited);
+            if(isElementOnRightValidAndNotVisited) {
+                minHeap.add(new HeapNode(matrix[currentRow+1][currentColumn], currentRow+1, currentColumn));
+                System.out.println("Visited:: " + matrix[currentRow+1][currentColumn]);
+                visited[currentRow+1][currentColumn] = 1;
+            }
+
+            boolean isElementOnBottomValidAndNotVisited = isElementOnBottomValid(currentMinimumElement, numCols, visited);
+            if(isElementOnBottomValidAndNotVisited) {
+                minHeap.add(new HeapNode(matrix[currentRow][currentColumn+1], currentRow, currentColumn+1));
+                System.out.println("Visited:: " + matrix[currentRow][currentColumn+1]);
+                visited[currentRow][currentColumn+1] = 1;
+            }
+        }
+        return lastRemovedElement.data;
+    }
+
+    private static boolean isElementOnRightValid(HeapNode currentMinimumElement, int numRows, int[][] visited) {
+        if(currentMinimumElement.row + 1 < numRows && visited[currentMinimumElement.row+1][currentMinimumElement.column] == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isElementOnBottomValid(HeapNode currentMinimumElement, int numCols, int[][] visited) {
+        if(currentMinimumElement.column + 1 < numCols && visited[currentMinimumElement.row][currentMinimumElement.column+1] == 0) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class HeapNode {
+    int data;
+    int row;
+    int column;
+
+    HeapNode(int data, int row, int column) {
+        this.data = data;
+        this.row = row;
+        this.column = column;
+    }
+}
---
+++ src/com/santosh/practise/strings/LongestPalindromeSubString.java
@@ -0,0 +1,18 @@
+package com.santosh.practise.strings;
+
+/**
+ * LeetCode problem 5
+ */
+public class LongestPalindromeSubString {
+    public static void main(String[] args) {
+        String s = "forgeeksskeegfor";
+        findLongestPalindromeSubString(s);
+    }
+
+    private static void findLongestPalindromeSubString(String s) {
+        int maxLength = 1;
+
+        //
+
+    }
+}
